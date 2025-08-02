2daf11eef80659f308b5f65800747a62d37530db
deuse-ftp <rdeusastico@gmail.com>
2025-08-01 21:59:31 -0300

    Atualiza m

--- src/App.tsx
+++ src/App.tsx
@@ -89,7 +89,7 @@ function App() {
             {isConnected && (
                 <div style={{ marginBottom: '20px' }}>
                     <p style={{ color: '#fff' }}>Connected: {getLastSixChars(address)}</p> {/* Substitui a frase original */}
-                    <p style={{ color: '#fff', marginTop: '10px' }}>If the message 'Loading game for the first time' appears, press F5!</p> {/* Nova mensagem */}
+                    <p style={{ color: '#fff', marginTop: '10px' }}>If the message ''Loading game for the first time'' appears, press F5 and please wait </p> {/* Nova mensagem */}
                     <button onClick={() => disconnect()} style={{ padding: '10px' }}>Desconectar</button>
                     {networkError && (
                         <div>
