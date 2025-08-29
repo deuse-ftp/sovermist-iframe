bd62e8e8578de3116cac03609f23a10b35db993b
deuse-ftp <rdeusastico@gmail.com>
2025-08-29 20:18:46 -0300

    m

--- src/App.tsx
+++ src/App.tsx
@@ -74,7 +74,7 @@ function App() {
     }
     setLoadingUsername(true);
     try {
-      const url = 'https://monad-games-id-site.vercel.app/api/check-wallet?wallet=' + walletAddress;
+      const url = 'https://monad-games-id-site.vercel.app/api/check-wallet?wallet=' + walletAddress.toLowerCase();
       console.log('ℹ️ Fetching username for:', walletAddress);
       const response = await fetch(url);
       const data = await response.json();
@@ -185,7 +185,7 @@ function App() {
           <p>If the message ''Loading game for the first time'' appears, press F5 and please wait</p>
           <iframe
             frameBorder="0"
-            src="https://itch.io/embed-upload/14806081?color=333333"
+            src="https://itch.io/embed-upload/14806680?color=333333"
             allowFullScreen
             width="1280"
             height="760"
